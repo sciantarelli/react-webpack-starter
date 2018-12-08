51bac33b55961e4c8d36e3effccd4aa98b0c9271
Scott Sciantarelli <ssciantarelli@iexposure.com>
2018-12-08 16:03:41 -0700

    Added custom webpack dev server with Express

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,18 +1,26 @@
 const path = require("path");
-console.log(__dirname);
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     main: "./src/main.js"
   },
+  mode: "development",
   output: {
     filename: "[name]-bundle.js",
     path: path.resolve(__dirname, "../build"),
+    publicPath: "/"
   },
   devServer: {
     contentBase: "build",
-    overlay: true // Shows errors directly in the browser window
+    overlay: true, // Shows errors directly in the browser window
+    hot: true,
+    stats: {
+      colors: true
+    }
   },
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -46,13 +54,6 @@ module.exports = {
         test: /\.html$/,
         use: [
           {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]"
-            }
-          },
-          { loader: "extract-loader" },
-          {
             loader: "html-loader",
             options: {
               attrs: ["img:src"]
@@ -61,5 +62,12 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
+// require("babel-runtime/regenerator"); TODO: Note sure if this is needed, doesn't seem like it
+require("webpack-hot-middleware/client?reload=true"); // Sent down to the client to setup the websocket connection
 require("./main.css");
-require("./images/fractal.jpg");
 require("./index.html");
 
 var a = async args => {
