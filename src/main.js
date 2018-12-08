f06dd9e833dca72710b8aea5c9972ed1a7e0ba5c
Scott Sciantarelli <ssciantarelli@iexposure.com>
2018-12-08 13:38:10 -0700

    Added babel-plugin-transform-runtime, babel-polyfill, babel-preset-env, adjusted .babelrc,  and code to test them

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ require("./main.css");
 require("./images/fractal.jpg");
 require("./index.html");
 
-var a = () => {
-  console.log("Testing Babel!")
-};
+var a = async args => {
+  const { a, b } = args;
+  await console.log('Babel Additions', a, b);
+}
 
-a();
+a({ a: 1, b: 2 });
