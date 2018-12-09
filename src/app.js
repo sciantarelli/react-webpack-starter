a2a38912d6555af6ba9a1589b71ee4fd9f2ad4f1
Scott Sciantarelli <ssciantarelli@iexposure.com>
2018-12-08 23:45:43 -0700

    Started adding React components, including a simple counter with increment

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Counter from './counter';
+import { hot } from 'react-hot-loader'
+
+const App = () => {
+  return(
+      <div id="main-container">
+        <h1>Some Fractal</h1>
+
+        <div>
+          <img src="./images/fractal.jpg" alt="fractal image" />
+        </div>
+
+        <Counter />
+      </div>
+  );
+};
+
+export default hot(module)(App);
--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 // require("babel-runtime/regenerator"); TODO: Note sure if this is needed, doesn't seem like it
+require("react-hot-loader/patch");
+require("babel-core/register"); // Get ES6 imports working
 require("webpack-hot-middleware/client?reload=true"); // Sent down to the client to setup the websocket connection
 require("./main.css");
+require("./images/fractal.jpg");
 require("./index.html");
-
-var a = async args => {
-  const { a, b } = args;
-  await console.log('Babel Additions', a, b);
-}
-
-a({ a: 1, b: 2 });
+require("./index");
